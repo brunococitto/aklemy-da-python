d2e277fb2038fb078bc58c82ea05196bdb3506c2
Bruno Cocitto <bcocitto@gmail.com>
2022-06-23 13:32:31 -0300

    removed scripts from main directory

--- init.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS main;
-CREATE TABLE main (id serial PRIMARY KEY, fecha_carga date, cod_localidad integer, id_provincia integer, id_departamento integer, categoria varchar, provincia varchar, localidad varchar, nombre varchar, domicilio varchar, codigo_postal varchar, telefono varchar, mail varchar, web varchar);
-DROP TABLE IF EXISTS main_info;
-CREATE TABLE main_info (id serial PRIMARY KEY, fecha_carga date, provincia varchar, categoria varchar, fuente varchar, cantidad integer);
-DROP TABLE IF EXISTS cinemas_info;
-CREATE TABLE cinemas_info (id serial PRIMARY KEY, fecha_carga date, provincia varchar, cantidad_pantallas integer, cantidad_butacas integer, cantidad_espacios_incaa integer);
